c6ba99c91194b7caa9d27addd6db0be30b6f08a7
durianno1 <durianno1@aliyun.com>
2020-05-23 00:22:54 +0800

    v1.0.3

--- goutilmod.go
+++ goutilmod.go
@@ -1,16 +1,9 @@
 package goutilmod
 
-import (
-	"fmt"
-)
-
 func max(a int, b int) int {
 	if a > b {
 		return a
 	} else {
 		return b
 	}
-}
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s", name)
 }
