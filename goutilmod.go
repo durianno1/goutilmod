7f9eac623c19d62975a6097f7526544a6b6471f8
durianno1 <durianno1@aliyun.com>
2020-05-23 00:21:54 +0800

    v1.0.2

--- goutilmod.go
+++ goutilmod.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 )
 
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s", name)
-}
-
 func max(a int, b int) int {
 	if a > b {
 		return a
 	} else {
 		return b
 	}
+}
+func Hi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
 }
